53148bd6cfd50e2eb2ac5072913515b6caf7de6c
Kutmanegmkl <kutmanegemkul0@gmail.com>
2025-10-24 21:08:33 +0600

    Friday commit

---
+++ thirdLesson.py
@@ -0,0 +1,35 @@
+# # #Структуры данных
+# # person=['Kutman','Egemkulov',31,"Jule",2002,'Student',75,187]
+# # print(f"Benim adim {person[0]}, soyadim {person[1]} dogum gunum{person[2]}. ")
+
+
+# # price=[input(int),input(str)]
+# # print("Change is",price)
+
+
+# p1=str(input("Биринчи эмне алгыныз келет?"))
+# p2=input("Кийин эмне алгыныз келет?")
+# p3=input('Дагы эмне алгыныз келет?')
+# p4=input("Дагы эмне аласыз?")
+# p5=input("Дагы аласызбы?")
+# p6=int(input("Баасы канча болсун?"))
+
+# pruduct=[p1,p2,p3,p4,p5,p6]
+# print("Сиздин тандаган продуктуларыныз:",pruduct[0,1,2,3,4], "Продуктулардын жалпы суммасы:",product[5])
+print(2*(3-1))
+a=5-1
+b=5+1
+print(a*b)
+
+a1=4-1
+print(0.3*a1)
+
+a3=91-1
+b3=2+1
+print(a3/b3)
+
+a4=5*4/3
+b4=a4**-2
+print(b4)
+
+print(1/3+5*(0.2-0.001)/91)
