b399d2a886cd66d76e369872289b20f7ee1e3025
Kutmanegmkl <kutmanegemkul0@gmail.com>
2025-10-29 21:01:02 +0600

    29-oktyabr, sharshembi

---
+++ lesson6.py
@@ -0,0 +1,92 @@
+# num1 =input("введите делитель  ")
+# num2 = input("введите делимое ")
+
+# try:
+#     num1 = int(num1) 
+#     num2 = int(num2)    # генерирует исключение ZeroDivisionError
+#     num3 = num1 / num2
+#     print("Результат деления:", num3)
+#     if num3 <= 5:
+#         print("num3 меньше 5")
+#     elif num3 > 5 and num3 < 10:
+#         print("num3 больше 5")
+#     else:
+#         print("num3 больше или равно 10")
+# except:
+#     print("Введите корректные числа")
+
+# finally:
+#     print("Блок try завершил выполнение")
+# print("Завершение программы")
+
+
+# a = 5
+# b = 6
+# sum1 = a+b
+# print(sum1)
+
+
+# word='peole'
+# text="Hello, my name is Sam"
+# list1=[2,-23,444,'name']
+
+# num1=int(input("Bir sayi gir:"))
+# if num1%2==0:
+#     print(f"{num1} жуп сан! ")
+# else:
+#     print(f"{num1} tak san!")
+
+
+#Problem id:B03-001
+# num2=float(input("bir sayi gir:"))
+# if num2>=0:
+#     print(f"{num2} positive number! ")
+# else:
+#     print(f"{num2} negative number!")
+
+
+#Problem id:B03-002
+
+# a=int(input("enter a number:"))
+# bl=a%2!=0
+# if bl==1:
+#     print(f"Число {a} является нечетным!")
+# else:
+#     print(f"Число {a} не является нечетным!")
+
+
+#Problem id:B03-003
+
+# a1=int(input("Bir sayi gir:"))
+# bl1=a1%2==0
+# if bl1==1:
+#     print(f"Число {a1} является четным!")
+# else:
+#     print(f"Число {a1} не является четным!")
+
+
+#Problem id:B03-004
+
+# a3=int(input("a'nin maanisi:"))
+# b3=int(input("b'nin maanisi:"))
+# bl3=a3>2
+# bl4=b3<=3
+# if bl3==1 and bl4==1:
+#     print(f"{a3}>2 jana {b3}<=3")
+# else:
+#     print(f"Barabarsizdik tuura emes! {a3}<=2 je {b3}>4")
+
+#Problem id:B03-005
+
+a4=int(input("a'nin maanisi:"))
+b4=int(input("b'nin maanisi:"))
+bl5=a4>=0
+bl6=b4<-2
+if bl5==1 or bl6==1:
+    print(f"{a4}>=0 je {b4}<-2")
+else:
+    print(f"{a4}<0 jana {b4}>=-2")
+
+
+
+
---
+++ seminar_3.py
@@ -0,0 +1,21 @@
+# #task_1
+# a=32
+# b=3.5
+# x=True
+# a_str=str(a)
+# b_str=str(b)
+# x_str=str(x)
+
+# #task_2
+# str_="Hello my friend!"
+
+# #task_3, task_4
+# name=input("Write your name:")
+# surname=input("Write your surname:")
+# res=name+","+surname
+# print(res)
+
+#task_5
+name1=input("Enter your name")
+score=int(input("Score"))
+print(f"Hello,{name1}! Your score is{score}")
